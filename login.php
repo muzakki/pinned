20690b735082a01d3aa50d7d5b3417341081d87a
Muzakki Faridzi <Muzakki Faridzi>
2015-12-08 06:50:04 +0700

    woooow

--- login.php
+++ login.php
@@ -10,41 +10,25 @@ $ousername = '';
 				if(isset($_POST['username'], $_POST['password']))
 				{
 					$username = mysql_real_escape_string($_POST['username']);
-					$password = $_POST['password'];
+					$passwordi = $_POST['password'];
+          $password = md5($passwordi);
 
 					$req = mysql_query('select u_password,id_user from user where u_username="'.$username.'"');
 					$dn = mysql_fetch_array($req);
 
-					if($dn['password']==sha1($password) and mysql_num_rows($req)>0)
+					if($dn['u_password']==$password)
 					{
 						$form = false;
 						$_SESSION['username'] = $_POST['username'];
-						$_SESSION['userid'] = $dn['id'];
-						if(isset($_POST['memorize']) and $_POST['memorize']=='yes')
-						{
-							$one_year = time()+(60*60*24*365);
-							setcookie('username', $_POST['username'], $one_year);
-							setcookie('password', sha1($password), $one_year);
-						}
+						$_SESSION['userid'] = $dn['id_user'];
+
 						echo '<h3>Login Sukses!</h3>';
-            header('index.php');
-					}
-					else
+            header('location:index.php');
+					} else
 					{
-						$form = true;
-						$message = 'Wrong username or password!!';
+            header('location:index.php?error=1');
 					}
-				}
-				else
-				{
-					$form = true;
-					$message = 'Fill the all fields!!';
-				}
-				if($form)
-				{
-					if(isset($message))
-					{
-						header('index.php?error=1');
-					}
-				}
+			} else {
+        header('location:index.php?error=1');
+			}
 ?>
